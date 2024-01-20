9d8c8004219f8bc5e5cadcc1ff464f3aa02f6522
Bleaze Perris <bperriscode@gmail.com>
2024-01-19 19:59:44 -0500

    rendered all pages

--- app.js
+++ app.js
@@ -8,17 +8,17 @@ app.listen(3000)
 
 app.get('/', (req, res) => {
     //res.send('<p>Home</p>')
-    res.sendFile('./src/index.html', {root: __dirname})
+    res.render('index')
 })
 
 app.get('/about', (req, res) => {
-    res.sendFile('./src/about.html', {root: __dirname})
+    res.render('about')
 })
 
-app.get('/about-us', (req, res) => {
-    res.redirect('/about')
+app.get('/blogs/create', (req, res) => {
+    res.render('create')
 })
 
 app.use((req, res) => {
-    res.status(404).sendFile('./src/404.html', {root: __dirname})
+    res.status(404).render('404.ejs')
 })
