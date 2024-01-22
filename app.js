58ebd41859890b54f7bfb8517323cf281ac98e9c
Bleaze Perris <bperriscode@gmail.com>
2024-01-22 15:11:33 -0500

    added routes and controllers

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const Blog = require('./models/blog')
+const blogRoutes = require('./routes/blogRoutes')
 
 const dbURI = 'mongodb+srv://test-user:testpass@cluster0.xw14m68.mongodb.net/?retryWrites=true&w=majority'
 mongoose.connect(dbURI)
@@ -12,7 +12,6 @@ mongoose.connect(dbURI)
 
 const app = express()
 app.set('view engine', 'ejs')
-app.set('views', 'src')
 
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended:true }))
@@ -25,30 +24,7 @@ app.get('/about', (req, res) => {
     res.render('about', { title: 'About'})
 })
 
-app.get('/blogs', (req, res) => {
-    Blog.find().sort({ createdAt: -1 })
-    .then((result) => {
-        res.render('index', { title: 'All Blogs', blogs: result })
-    })
-    .catch((err) => {
-        console.log(err)
-    })
-})
-
-app.post('/blogs', (req, res) => {
-    const blog = new Blog(req.body)
-    blog.save()
-        .then(() => {
-            res.redirect('/blogs')
-        })
-        .catch((err) => {
-            console.log(err)
-        })
-})
-
-app.get('/blogs/create', (req, res) => {
-    res.render('create', { title: 'Create a New Blog'})
-})
+app.use('/blogs', blogRoutes)
 
 app.use((req, res) => {
     res.status(404).render('404.ejs', { title: '404'})
