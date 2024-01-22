c1df32e78d3e941297569602d06467cdc1c8d24f
Bleaze Perris <bperriscode@gmail.com>
2024-01-21 20:59:55 -0500

    added mongodb database for blog objects and post blog method

--- app.js
+++ app.js
@@ -1,24 +1,49 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const Blog = require('./models/blog')
+
+const dbURI = 'mongodb+srv://test-user:testpass@cluster0.xw14m68.mongodb.net/?retryWrites=true&w=majority'
+mongoose.connect(dbURI)
+    .then((result) => {
+        console.log('connected to db')
+        app.listen(3000)
+    })
+    .catch((err) => console.log(err))
 
 const app = express()
 app.set('view engine', 'ejs')
 app.set('views', 'src')
 
-app.listen(3000)
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended:true }))
 
 app.get('/', (req, res) => {
-
-    const blogs = [
-        {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-        {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-        {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-      ];
-
-    res.render('index', { title: 'Home', blogs})
+  res.redirect('/blogs')
 })
 
 app.get('/about', (req, res) => {
     res.render('about', { title: 'About'})
+})
+
+app.get('/blogs', (req, res) => {
+    Blog.find().sort({ createdAt: -1 })
+    .then((result) => {
+        res.render('index', { title: 'All Blogs', blogs: result })
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+})
+
+app.post('/blogs', (req, res) => {
+    const blog = new Blog(req.body)
+    blog.save()
+        .then(() => {
+            res.redirect('/blogs')
+        })
+        .catch((err) => {
+            console.log(err)
+        })
 })
 
 app.get('/blogs/create', (req, res) => {
